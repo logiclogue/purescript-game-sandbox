a235fbbe603a8d1134ad3d9a77d954f5b44de978
logiclogue <jordanpaullord@hotmail.co.uk>
2018-08-07 23:00:49 +0100

    Sets up webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    devtool: "source-maps",
+    entry: {
+        main: "./src/index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+};
